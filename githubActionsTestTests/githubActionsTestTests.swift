6424900903f3f2a581dda7a8825c2374a7bd3f25
MahmoudZiko <deep2012_1st@yahoo.com>
2025-07-07 18:25:55 +0300

    Update githubActionsTestTests.swift

--- githubActionsTestTests/githubActionsTestTests.swift
+++ githubActionsTestTests/githubActionsTestTests.swift
@@ -17,13 +17,13 @@ struct githubActionsTestTests {
     @Test
     func testSumWithIntegers() {
         let calculator = Calculator(num1: 3, num2: 4)
-        #expect(calculator.sum() == 9)
+        #expect(calculator.sum() == 7)
     }
 
     @Test
     func testSumWithDoubles() {
         let calculator = Calculator(num1: 2.5, num2: 3.1)
-        #expect(calculator.sum() == 5.116)
+        #expect(calculator.sum() == 5.6)
     }
 
 }
