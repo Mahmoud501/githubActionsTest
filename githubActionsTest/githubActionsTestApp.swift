35f1720547b4dc34c5d8ddd4940855a7f4b9b8bf
MahmoudZiko <deep2012_1st@yahoo.com>
2025-07-07 13:47:51 +0300

    Initial Commit

---
+++ githubActionsTest/githubActionsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubActionsTestApp.swift
+//  githubActionsTest
+//
+//  Created by Mahmoud Zaki on 07/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct githubActionsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
