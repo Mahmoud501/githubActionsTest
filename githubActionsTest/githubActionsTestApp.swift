bd36ed2f10f74337af9179f0a8ce57d1d24e8df9
MahmoudZiko <deep2012_1st@yahoo.com>
2025-07-09 19:42:23 +0300

    iq

--- githubActionsTest/githubActionsTestApp.swift
+++ githubActionsTest/githubActionsTestApp.swift
@@ -6,12 +6,20 @@
 //
 
 import SwiftUI
+import IQKeyboardManagerSwift
+import IQKeyboardToolbarManager
 
 @main
 struct githubActionsTestApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear {
+                    IQKeyboardManager.shared.isEnabled = true
+                    IQKeyboardToolbarManager.shared.isEnabled = true
+                    IQKeyboardToolbarManager.shared.toolbarConfiguration.placeholderConfiguration.showPlaceholder = true
+                    IQKeyboardToolbarManager.shared.toolbarConfiguration.previousNextDisplayMode = .alwaysShow
+                }
         }
     }
 }
