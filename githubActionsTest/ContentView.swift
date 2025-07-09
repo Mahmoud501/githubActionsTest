5c613c0f88ed85b14a5181c96c2bfdb0f475c6a9
MahmoudZiko <deep2012_1st@yahoo.com>
2025-07-09 15:48:37 +0300

    Update ContentView.swift

--- githubActionsTest/ContentView.swift
+++ githubActionsTest/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Mahmoud Zaki Hello CI/CD!")
         }
         .padding()
     }
